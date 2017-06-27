fd8be0b3cb9f7610e2750d1ba16a8fb41e27c20a
James Cook <jamesjcook@users.noreply.github.com>
2017-06-27 12:29:22 -0400

    Finished first functional version.

--- AtlasViewerLaunch.bat
+++ AtlasViewerLaunch.bat
@@ -1,16 +1,12 @@
+@echo off
+SETLOCAL ENABLEEXTENSIONS
+SETLOCAL EnableDelayedExpansion
 @REM AtlasViewerLaunch.bat
-@close existing sessions
-taskkill /im AtlasViewerApp-real.exe
+@REM close existing sessions
+taskkill /im AtlasViewerApp-real.exe > NUL 2>&1
 start %~dp0\session_cleanup.bat atlasviewer.exe 8
-set file=%1
-FOR %%i IN ("!file!") DO (
-ECHO filedrive=%%~di
-ECHO filepath=%%~pi
-set filepath=%%~pi
-ECHO filename=%%~ni
-ECHO fileextension=%%~xi
-)
+set filepath=%~p1
 cd %filepath%
-timeout 3
-call %*
-exit 
+
+start /wait %*
+exit \b
