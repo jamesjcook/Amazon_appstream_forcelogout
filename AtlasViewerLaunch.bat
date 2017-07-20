b73bd2cb18d0beaeb48257df59668ba26c7b93ab
James Cook <jamesjcook@users.noreply.github.com>
2017-07-20 13:05:54 -0400

    Added additional prints to make it clear what is happening.

--- AtlasViewerLaunch.bat
+++ AtlasViewerLaunch.bat
@@ -3,10 +3,16 @@ SETLOCAL ENABLEEXTENSIONS
 SETLOCAL EnableDelayedExpansion
 @REM AtlasViewerLaunch.bat
 @REM close existing sessions
+
+echo Closing existing atlasviewerapp-real
 taskkill /f /im AtlasViewerApp-real.exe > NUL 2>&1
+
+echo Setting up session cleanup for atlasviewer.exe
 start %~dp0\session_cleanup.bat atlasviewer.exe 8
+
 set filepath=%~p1
 cd %filepath%
+echo %* >> c:\CIVM_Apps\AtlasViewerLaunch.txt
 echo %* > c:\CIVM_Apps\AppLaunch.txt
 start /wait %*
 exit \b
