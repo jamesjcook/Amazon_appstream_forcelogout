32e17e9ddb5ba2d481ccdf2657a4843734d638db
James Cook <jamesjcook@users.noreply.github.com>
2018-03-21 10:40:01 -0400

    Removed git-bash as its not universal

--- AppStreamRunner.bat
+++ AppStreamRunner.bat
@@ -9,8 +9,8 @@ set var=%%f
 
 
 if not exist c:\IP\session_vars.txt (
-  echo ESSION_RETRIEVE_FAILURE!!!! ENTERING DEBUG!
-  start cmd & start notepad & start explorer & start git-bash --cd-to-home
+  echo SESSION_RETRIEVE_FAILURE!!!! ENTERING DEBUG!
+  start cmd & start notepad & start explorer 
 ) else (
   echo session startup
   type c:\IP\session_var.txt
