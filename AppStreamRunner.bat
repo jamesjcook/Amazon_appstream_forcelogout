65af92fc786612a11ea946774d26c0a2e904bfbc
James Cook <jamesjcook@users.noreply.github.com>
2018-03-21 10:38:36 -0400

    Cleaned up redundatnt timeouts.

--- AppStreamRunner.bat
+++ AppStreamRunner.bat
@@ -11,10 +11,10 @@ set var=%%f
 if not exist c:\IP\session_vars.txt (
   echo ESSION_RETRIEVE_FAILURE!!!! ENTERING DEBUG!
   start cmd & start notepad & start explorer & start git-bash --cd-to-home
-  timeout 2
 ) else (
   echo session startup
   type c:\IP\session_var.txt
-  timout 2
 )
+
+timeout 2
 exit \b
