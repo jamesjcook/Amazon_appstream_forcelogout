b20a32c3cb0016eb284eba43cfbf1ef37aca170b
James Cook <jamesjcook@users.noreply.github.com>
2018-11-16 13:48:10 -0500

    Fixed typeo with var dump, and added timeout to give time for things to happen.

--- AppStreamRunner.bat
+++ AppStreamRunner.bat
@@ -7,13 +7,13 @@ set var=%%f
 )
 %var%
 
-
+timeout 4
 if not exist c:\IP\session_vars.txt (
   echo SESSION_RETRIEVE_FAILURE!!!! ENTERING DEBUG!
   start cmd & start notepad & start explorer 
 ) else (
   echo session startup
-  type c:\IP\session_var.txt
+  type c:\IP\session_vars.txt
 )
 
 timeout 2
