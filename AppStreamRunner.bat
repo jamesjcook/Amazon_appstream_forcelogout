8faa31b75e5a0bbb4cd2626e25a1d7e319a6c844
James Cook <jamesjcook@users.noreply.github.com>
2018-11-20 13:09:06 -0500

    Changed runner to run context.

--- AppStreamRunner.bat
+++ AppStreamRunner.bat
@@ -2,25 +2,24 @@
 @SETLOCAL ENABLEEXTENSIONS
 @SETLOCAL EnableDelayedExpansion
 
-@call timestamp >> c:\IP\sesh.txt
-@c:\IP\SessionContextRetriever.exe >> c:\IP\sesh.txt
+@set PATH=%PATH%;%~dp0
+@set sesh_run=c:\ip\sesh.bat
+
+@REM dump sessioncontext to a test file plainly first
+@REM we'll test that file for empty later.
+@c:\IP\SessionContextRetriever.exe >> %sesh_run%test
+echo @SETLOCAL ENABLEEXTENSIONS > %sesh_run%
+echo @SETLOCAL EnableDelayedExpansion >> %sesh_run%
+@c:\IP\SessionContextRetriever.exe >> %sesh_run%
 
 @echo off
-@REM added quotes around var set to better protect it, however that may not be our problem at all!
-@REM Could have been that we accidentially had code in our launch params and WE NEED IT TO BE EMPTY.
-for /f "tokens=* USEBACKQ" %%f in (`c:\IP\SessionContextRetriever.exe`) do (
-set "var=%%f"
+(
+    for /f usebackq^ eol^= %%a in (
+        %sesh_run%test
+        ) do break
+) && (
+    start /wait %sesh_run%
+) || (
+    echo session context failure & start cmd & start notepad & start explorer
 )
-%var%
-
-timeout 4
-if not exist c:\IP\session_vars.txt (
-  echo SESSION_RETRIEVE_FAILURE!!!! ENTERING DEBUG!
-  start cmd & start notepad & start explorer 
-) else (
-  echo session startup
-  type c:\IP\session_vars.txt
-)
-
-timeout 2
-exit \b
+exit /b
